7c7eb759b588939a566838332f3455c1969b8619
developer <igamblingsites@gmail.com>
2024-11-07 17:29:54 +0530

    Fixed design issues

--- src/app/flight-deck-api/page.js
+++ src/app/flight-deck-api/page.js
@@ -12,7 +12,7 @@ import { BuiltFlight } from "../_DeckComponents/BuiltFlight";
 
 export default function FlightDeckApi() {
   return (
-    <main className="">
+    <main className="bg-lightbg bg-cover">
       <Header />
       <section className="pt-32 lg:pt-36 relative px-4 bg-banner-bg bg-bottom bg-no-repeat b bg-cover">
         <div className="container mx-auto">
